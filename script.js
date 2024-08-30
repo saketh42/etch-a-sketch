de5a40c82ec741b19334d24fce5ded78110926d8
Atarashi <116485868+saketh42@users.noreply.github.com>
2024-08-30 19:51:58 +0530

    Added multiple colors

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const grid = document.querySelector(".grid");
 const button = document.querySelector(".grid-size-button");
 const boxes = document.querySelectorAll('.box');
 const reset = document.querySelector(".reset");
+const colors = document.querySelectorAll(".color");
 
 // The length of the grid
 const gridSide = grid.offsetHeight - 4;
 
 // The default number of boxes
 let size = 16;
+let color = "#2ecf35";
 
 // To change the size of the grid
 button.addEventListener("click", () => {
@@ -19,10 +21,28 @@ button.addEventListener("click", () => {
     }
 });
 
+// Reset Grid
 reset.addEventListener("click", () => {
     generateBox();
 });
 
+// Color changing
+colors.forEach(colorElement => {
+    colorElement.addEventListener("click", () => {
+        if (colorElement.classList.contains("red")) {
+            color = "#cf352e";
+        } else if (colorElement.classList.contains("blue")) {
+            color = "#352ecf"; 
+        } else if (colorElement.classList.contains("green")) {
+            color = "#2ecf35"; 
+        } else if (colorElement.classList.contains("black")) {
+            color = "#000000"; 
+        } else if (colorElement.classList.contains("white")) {
+            color = "#ffffff"; 
+        }
+    });
+});
+// Grid generation
 function generateBox() {
     const boxSide = gridSide / size - 2;
 
@@ -42,17 +62,16 @@ function generateBox() {
         grid.appendChild(box);
     }
 
-    const boxes = document.querySelectorAll(".box");
+    const boxes = document.querySelectorAll('.box');
+
+    //Coloring when hover
     boxes.forEach((box) => {
-        box.addEventListener('mouseout', () => {
-            if (box.style.backgroundColor == "white") {
-                box.style.backgroundColor = "black";
-                box.style.opacity = "0.2";
-            } else {
-                let currentOpacity = parseFloat(box.style.opacity);
-                if (currentOpacity < 1) {
-                    box.style.opacity = (currentOpacity + 0.2).toString();
-                }
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = color;
+
+            let currentOpacity = parseFloat(box.style.opacity) || 0; // Get current opacity
+            if (currentOpacity < 1) {
+                box.style.opacity = (currentOpacity + 0.2).toFixed(1);
             }
         });
     });
